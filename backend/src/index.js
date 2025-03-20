c89952b8102c369689e2355d9466f59b1794358c
si-ck <sikandervali2004@gmail.com>
2025-03-20 22:46:57 +0530

    backend server api issues.....!!!!

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,8 +7,3 @@ dotenv.config(); // Load environment variables
 // Connect to MongoDB
 connectDB();
 
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`⚙️ Server is running at port : ${PORT}`);
-});
