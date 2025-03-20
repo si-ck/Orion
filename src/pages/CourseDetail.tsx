7a6af6d80cd77002c80852a6b720d839b9f5e2d2
si-ck <sikandervali2004@gmail.com>
2025-03-21 02:32:43 +0530

    dark mode applied

--- src/pages/CourseDetail.tsx
+++ src/pages/CourseDetail.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from '@tanstack/react-query'
 import { 
   BarChart, 
   Globe, 
